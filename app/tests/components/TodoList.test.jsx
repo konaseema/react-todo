e2cbe14ed014a6750b5091c1915b37e15b82516a
Vasisht A. Shastry <shastryucsb@gmail.com>
2016-11-02 20:44:17 -0500

    style todo application

--- app/tests/components/TodoList.test.jsx
+++ app/tests/components/TodoList.test.jsx
@@ -18,15 +18,23 @@ describe('TodoList', () => {
             id: 1,
             text: 'a'
         },
-            {
-                id: 2,
-                text: 'b'
-            }, {
-                id: 3,
-                text: 'c'
-            }];
-        var todoList = TestUtils.renderIntoDocument(<TodoList todos={todos}/>);
+        {
+            id: 2,
+            text: 'b'
+        }, {
+            id: 3,
+            text: 'c'
+        }];
+        var todoList = TestUtils.renderIntoDocument(<TodoList todos={todos} />);
         var todosComponents = TestUtils.scryRenderedComponentsWithType(todoList, Todo);
         expect(todosComponents.length).toBe(todos.length);
     });
+
+    it('should render empty message if no todos', () => {
+        var todos = [];
+        var todoList = TestUtils.renderIntoDocument(<TodoList todos={todos} />);
+        var $el = $(ReactDOM.findDOMNode(todoList));
+        expect($el.find('.container__message').length).toBe(1);
+    });
+
 });
