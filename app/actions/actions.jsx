16479c57e1d81c4d3a03f21ff8587165502cc086
Vasisht A. Shastry <shastryucsb@gmail.com>
2016-11-30 13:11:24 -0600

    add action generators and tests

---
+++ app/actions/actions.jsx
@@ -0,0 +1,19 @@
+export var setSearchText = (searchText) => {
+    return {
+        type: 'SET_SEARCH_TEXT', searchText //ES6 notation
+    };
+};
+
+export var addTodo = (text) => {
+    return {
+        type: 'ADD_TODO', text //ES6 notation
+    };
+};
+
+export var toggleShowCompleted = () => {
+    return {type: 'TOGGLE_SHOW_COMPLETED'};
+};
+
+export var toggleTodo = (id) => {
+    return {type: 'TOGGLE_TODO', id} //ES6 notation;
+};
