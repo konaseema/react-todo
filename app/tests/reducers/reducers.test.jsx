127626349d58fdbece4c49a9d4b213542cd6decd
Vasisht A. Shastry <shastryucsb@gmail.com>
2016-12-01 18:20:25 -0600

    add toggleTodo reducer and accompanying tests

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -23,4 +23,62 @@ describe('Reducers', () => {
             expect(res).toEqual(true)
         });
     });
+
+    describe('todosReducer', () => {
+        it('should add new todo', () => {
+            var action = {
+                type: 'ADD_TODO',
+                text: 'Walk the dog'
+            };
+
+            var res = reducers.todosReducer(df([]), df(action));
+            expect(res.length).toEqual(1);
+            expect(res[0].text).toEqual(action.text);
+        });
+
+        it('should toggle todo properly', () => {
+            // define todo array with todo item generate action call reducer and assert
+            // complete flipped
+
+            var todos = [
+                {
+                    id: 11,
+                    text: 'test features',
+                    completed: false,
+                    createdAt: 0,
+                    completedAt: undefined
+                }
+            ];
+
+            var action = {
+                type: 'TOGGLE_TODO',
+                id: 11
+            };
+            var res = reducers.todosReducer(todos, action);
+            expect(res[0].completed).toExist();
+            expect(res[0].completedAt).toExist();
+        });
+
+        it('should toggle completed value to undefined when completed changed from true to f' +
+                'alse',
+        () => {
+            var todos = [
+                {
+                    id: 11,
+                    text: 'test features',
+                    completed: true,
+                    createdAt: 0,
+                    completedAt: 1234
+                }
+            ];
+
+            var action = {
+                type: 'TOGGLE_TODO',
+                id: 11
+            };
+            var res = reducers.todosReducer(todos, action);
+            expect(res[0].completed).toBe(false);
+            expect(res[0].completedAt).toNotExist();
+        });
+    });
 });
