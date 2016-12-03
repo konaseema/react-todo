cbab0347cf5c4c1a0c2eeb706581d3dd8664ac6d
Vasisht A. Shastry <shastryucsb@gmail.com>
2016-12-03 01:25:27 -0600

    refactor TodoSearch with Redux Store

--- app/components/TodoList.jsx
+++ app/components/TodoList.jsx
@@ -1,22 +1,25 @@
 var React = require('react');
 import Todo from 'Todo';
 var {connect} = require('react-redux');
+var TodoAPI = require('TodoAPI');
 
 export var TodoList = React.createClass({
     render: function () {
-        var {todos} = this.props;
+        var {todos, showCompleted, searchText} = this.props;
         var renderTodos = () => {
             if (todos.length === 0) {
                 return (
                     <p className="container__message">Nothing to do</p>
                 )
             }
-            return todos.map((todo) => {
-                return (
-                    <Todo key={todo.id} {...todo} />
+            return TodoAPI
+                .filterTodos(todos, showCompleted, searchText)
+                .map((todo) => {
+                    return (
+                        <Todo key={todo.id} {...todo}/>
                     /* Alternative: <Todo key={todo.id} id={todo.id} text={todo.text}/>*/
-                )
-            });
+                    )
+                });
         };
 
         return (
@@ -24,13 +27,9 @@ export var TodoList = React.createClass({
                 {renderTodos()}
             </div>
         )
-    },
+    }
 });
 
-export default connect(
-    (state) => {
-        return {
-            todos: state.todos
-        }
-    }
-)(TodoList);
+export default connect((state) => {
+    return state;
+})(TodoList);
--- app/tests/components/TodoSearch.test.jsx
+++ app/tests/components/TodoSearch.test.jsx
@@ -4,7 +4,7 @@ var expect = require('expect');
 var $ = require('jQuery');
 var TestUtils = require('react-addons-test-utils');
 
-var TodoSearch = require('TodoSearch')
+import {TodoSearch} from 'TodoSearch';
 
 describe('TodoSearch', () => {
 
@@ -12,23 +12,29 @@ describe('TodoSearch', () => {
         expect(TodoSearch).toExist();
     });
 
-    it('should call onSearch witn entered input text', () => {
-        var searchText = 'dog'
+    it('should dispatch SET_SEARCH_TEXT on input change', () => {
+        var searchText = 'dog';
+        var action = {
+            type: 'SET_SEARCH_TEXT',
+            searchText
+        }
         var spy = expect.createSpy();
-        var todosearch = TestUtils.renderIntoDocument(<TodoSearch onSearch={spy}/>);
+        var todosearch = TestUtils.renderIntoDocument(<TodoSearch dispatch={spy}/>);
 
         todosearch.refs.searchText.value = searchText;
         TestUtils.Simulate.change(todosearch.refs.searchText);
-        expect(spy).toHaveBeenCalledWith(false, searchText);
+        expect(spy).toHaveBeenCalledWith(action);
     });
 
-    it('should call onSearch witn entered proper checked value', () => {
+    it('should dispatch TOGGLE_SHOW_COMPLETED when checkbox checked', () => {
+        var action = {
+            type: 'TOGGLE_SHOW_COMPLETED'
+        }
         var spy = expect.createSpy();
-        var todosearch = TestUtils.renderIntoDocument(<TodoSearch onSearch={spy}/>);
+        var todosearch = TestUtils.renderIntoDocument(<TodoSearch dispatch={spy}/>);
 
         todosearch.refs.showCompleted.checked = true;
         TestUtils.Simulate.change(todosearch.refs.showCompleted);
-        expect(spy).toHaveBeenCalledWith(true, '');
-
+        expect(spy).toHaveBeenCalledWith(action);
     });
 });
